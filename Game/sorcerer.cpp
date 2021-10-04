fa537128d602ec042e03388f6347b789d5f0e23a
garretto1003 <46901531+garretto1003@users.noreply.github.com>
2021-10-04 13:17:06 -0500

    Create sorcerer.cpp

---
+++ Game/sorcerer.cpp
@@ -0,0 +1,7 @@
+#include "Sorcerer.h"
+
+// Constructor(s) ===========================
+// Sorcerer Name To Base Main
+Sorcerer::Sorcerer(std::string sorcerer_name)
+    : Warrior("Sorcerer", sorcerer_name, 10, 5) { }
+// ==========================================
